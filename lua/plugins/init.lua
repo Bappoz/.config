ffe770d864650dfdb53635a23606f0decb0b23dc
bappo <landradezanetti@gmail.com>
2026-02-16 00:19:43 -0300

    feat: add copilot and node config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,6 +12,44 @@ return {
       require "configs.lspconfig"
     end,
   },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = "<C-l>", -- Use Ctrl + l para aceitar a sugestão
+          },
+        },
+      })
+    end,
+  },
+
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "main",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" },
+      { "nvim-lua/plenary.nvim" },
+    },
+    build = "make tiktoken", -- Opcional, ajuda na performance
+    opts = {
+      debug = false,
+    },
+    keys = {
+      { "<leader>cc", "<cmd>CopilotChatToggle<cr>", desc = "Abrir Chat do Copilot" },
+      { "<leader>ce", "<cmd>CopilotChatExplain<cr>", desc = "Explicar Código" },
+      { "<leader>cr", "<cmd>CopilotChatRewrite<cr>", desc = "Reescrever Código"},
+      { "<leader>cf", "<cmd>CopilotChatFix<cr>", desc = "Corrigir Código" },
+    },
+  },
+
+
+
 
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
